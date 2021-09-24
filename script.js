a37235d60137a27aa6f6c785751ec0b6af51dc7f
zydrunaszujus <zydriuszu@gmail.com>
2021-09-24 23:54:51 +0300

    Pridėta papildomų nuotraukų
    Pridėta nuotraukų;
    Perdarytas langų atsidarymas iš JS;
    Pradėta kurti variklių papildomi mygtukų apršymai.

--- script.js
+++ script.js
@@ -1,6 +1,36 @@
-function manoMygtukas() {
-    var vienfazis = document.getElementById("vienosFazes");
-    document.getElementById("vienosFazes").innerHTML = 
+function myFunction1() {
+     window.open("link/varikliai.html");
+    
+  }
+  function myFunction2() {
+    window.open("link/kabeliai.html");
+  }
+  function myFunction3() {
+    window.open("link/keitikliai.html");
+  }
+  function myFunction4() {
+    window.open("link/paleidejai.html");
+  }
+  function myFunction5() {
+    window.open("link/reles.html");
+  }
+  function myFunction6() {
+    window.open("link/davikliai.html");
   }
   
+  // function sinchroniniai(){
+  //   var element = document.getElementById("sinchron");
+  //   emitEvent(element, 'sinch');
+  // }
   
+  function sinchroniniai() {
+    if ( document.getElementById ) {
+    target = document.getElementById("sinchron");
+    if ( target.style.display == "none" ) {
+    target.style.display = "";
+    } else {
+    target.style.display = "none";
+    }
+    }
+   }
+   
